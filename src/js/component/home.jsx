b12ecfdd1fdbbccf6ba6b6c920707b904993e847
DGgithubDG <thenumerousrandom@gmail.com>
2023-03-13 14:10:17 +0000

    ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,38 +9,30 @@ import React from "react";
 
 import Footer from "./footer";
 import Navbar from "./navbar"
+import casino from "../../img/casino.9.jpg";
+
 //import ResponsiveImage from "./img";
 //import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-        <div className="Background">
+        <div className="">
     <Navbar />
-    <div></div><div>
+    <img class="img-fluid" src={casino}></img> 
     <div class="card text-center">
   <div class="card-body">
     <h5 class="card-title"><h1>PEGASINO</h1></h5>
     {/* <BG title="Bulgaria" className="falg"/> */}
-
-
     <p class="card-text text-start"><strong>Always the best odds with Casino</strong></p>
     <p class="card-text text-start"><strong>Quick and easy deposits & withdrawals</strong></p>
     <p class="card-text text-start"><strong>Transparency, responsibility and security</strong></p>
-   
-    <a href="#" class="button btn btn-success">REGISTER</a>
-  
-    {/* <Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link> */}
+    <button type="button" class="btn btn-success">REGISTER</button>
   </div>
 </div>
-    {/* <img className="Back" src="https://www.wildz.com/cdn/2021/09/30/wildz-blog-online-casino-2_uid_6155c3688c52d.jpeg?&cover=true" class="img-fluid" alt=""/>  */}
-     {/* <Card/>  */}
-    </div>
-    <Footer/>
-    </div>
+<Footer/>
 
+</div>
     
     
 	);
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,42 +2,46 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <div class="container-fluid">
+      <a class="navbar-brand" href="#"><h1><strong>PEGASINO</strong></h1></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
+            <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>SPORTS</i></button></div></a>
+            </li>
+            <li class="nav-item">
+            <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>CASINO</i></button></div></a>
+            </li>
+            <li class="nav-item dropdown">
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li> class="dropdown-divider"</li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>LIVE CASINO</i></button></div></a>
+            </li>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li class="nav-item">
+            <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>POKER</i></button></div></a>
+            </li>
+               </ul>
+            </li>
+            <li class="nav-item">
+            <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>VIRTUAL GAMES</i></button></div></a>
+            </li>
+        
+        
+        <li class="nav-item">
+            <a className=" sportsicon nav-link active" aria-current="page" href="https://www.pegasino.com/"><div className="navnav"><button type="button" class="btn btn-dark"><div className="pad"></div><i>BLOG</i></button></div></a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+          <form class="d-flex">
+        <div className="Login"><button type="button" className="btn btn-success">REGISTER</button></div>
+        <div className="Register"><button type="button" className="btn btn-success">LOGIN</button></div>
+          </form>
+        </div>
+      </div>
+    </nav>
     )
 }
 
