a38fe4b41966851b21ec6e3a24a4e4442bfe665b
DGgithubDG <thenumerousrandom@gmail.com>
2023-03-15 09:16:47 +0000

    registration form

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,24 +12,85 @@ import Navbar from "./navbar"
 import casino from "../../img/casino.9.jpg";
 
 //import ResponsiveImage from "./img";
-//import rigoImage from "../../img/rigo-baby.jpg";
+import logo from "../../img/casino.9.jpg";
 
 //create your first component
 const Home = () => {
 	return (
         <div className="">
     <Navbar />
-    <img class="img-fluid" src={casino}></img> 
-    <div class="card text-center">
-  <div class="card-body">
-    <h5 class="card-title"><h1>PEGASINO</h1></h5>
-    {/* <BG title="Bulgaria" className="falg"/> */}
-    <p class="card-text text-start"><strong>Always the best odds with Casino</strong></p>
-    <p class="card-text text-start"><strong>Quick and easy deposits & withdrawals</strong></p>
-    <p class="card-text text-start"><strong>Transparency, responsibility and security</strong></p>
-    <button type="button" class="btn btn-success">REGISTER</button>
+    <section class="vh-100">
+  <div class="container h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-lg-12 col-xl-11">
+        <div class="card text-black" >
+          <div class="card-body p-md-5">
+            <div class="row justify-content-center">
+              <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                <form class="mx-1 mx-md-4">
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="text" id="form3Example1c" class="form-control" />
+                      <label class="form-label" for="form3Example1c">Your Name</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="email" id="form3Example3c" class="form-control" />
+                      <label class="form-label" for="form3Example3c">Your Email</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4c" class="form-control" />
+                      <label class="form-label" for="form3Example4c">Password</label>
+                    </div>
+                  </div>
+
+                  <div class="d-flex flex-row align-items-center mb-4">
+                    <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                    <div class="form-outline flex-fill mb-0">
+                      <input type="password" id="form3Example4cd" class="form-control" />
+                      <label class="form-label" for="form3Example4cd">Repeat your password</label>
+                    </div>
+                  </div>
+
+                  <div class="form-check d-flex justify-content-center mb-5">
+                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                    <label class="form-check-label" for="form2Example3">
+                      I agree all statements in <a href="#!">Terms of service</a>
+                    </label>
+                  </div>
+
+                  <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="button" class="btn btn-primary btn-lg">Register</button>
+                  </div>
+
+                </form>
+
+              </div>
+              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+              <img class="img-fluid" src={logo}></img> 
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
+</section>
 <Footer/>
 
 </div>
